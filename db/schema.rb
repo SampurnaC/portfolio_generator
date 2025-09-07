00ff7428d861e9b9587ca48bc9601cf202d1e807
sam <sampurna.chapagainnn@gmail.com>
2025-09-07 23:12:38 +0100

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_06_221840) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_07_220037) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "portfolios", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.string "slug"
+    t.string "bio"
+    t.string "theme"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_portfolios_on_user_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.bigint "portfolio_id", null: false
+    t.string "title"
+    t.string "summary"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["portfolio_id"], name: "index_projects_on_portfolio_id"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +52,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_06_221840) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "portfolios", "users"
+  add_foreign_key "projects", "portfolios"
 end
