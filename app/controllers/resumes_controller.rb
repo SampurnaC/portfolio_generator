595d1029377b82121b485ac3a171f748da0e570b
sam <sampurna.chapagainnn@gmail.com>
2025-09-11 18:22:07 +0100

    resumes controller crud operation

---
+++ app/controllers/resumes_controller.rb
@@ -0,0 +1,62 @@
+class ResumesController < ApplicationController
+  before_action :set_resume, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_resume!, only: [:edit, :update, :destroy]
+
+  def index
+    @resumes=Resume.all
+  end
+
+  def show
+  end
+
+  def new
+    if current_user.resume.present?
+      redirect_to edit_user_resume_path(current_user, current_user.resume)
+    else
+      @resume=current_user.build_resume
+      2.times {@resume.projects.build}
+    end
+  end
+
+  def create
+    @resume=current_user.build_resume(resume_params)
+    if @resume.save
+      redirect_to root_path, notice: "Resume created successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @resume.update(resume_params)
+      redirect_to user_resumes_path, notice: "Resume updated successfully"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @resume.destroy
+    redirect_to root_path, notice: "Resume deleted"
+  end
+
+  private
+
+  def set_resume
+    @resume = Resume.find(params[:id])
+  end
+
+  def authorize_resume!
+    unless @resume.user==current_user
+      redirect_to root_path, alert: "Not authorized to edit this resume"
+    end
+  end
+
+  def resume_params
+    params.require(:resume).permit(:name, :slug, :bio, :theme, projects_attributes: [:id, :title, :summary, :url, :_destroy])
+  end
+
+end
