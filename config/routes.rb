3820901caaed5f16cfcb77f2548b839602043e6a
sam <sampurna.chapagainnn@gmail.com>
2025-09-11 18:06:48 +0100

    update portfolio by resume in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :users do
-    resources :portfolios
+    resources :resumes
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
 
+  root "resumes#index"
   
 end
