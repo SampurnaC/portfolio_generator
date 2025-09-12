d8f25f1ea545fbbd716b558d9803176b6ff2875a
sam <sampurna.chapagainnn@gmail.com>
2025-09-12 07:48:45 +0100

    add relationshio with resume and skills

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,6 +1,8 @@
 class Resume < ApplicationRecord
   belongs_to :user
   has_many :projects, dependent: :destroy
+  has_many :skills, dependent: :destroy
 
   accepts_nested_attributes_for :projects, allow_destroy: true
+  accepts_nested_attributes_for :skills, allow_destroy: true
 end
