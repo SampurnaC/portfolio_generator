bb4d03857d19f9fcd55d70b7da4a09328c0775c3
sam <sampurna.chapagainnn@gmail.com>
2025-09-12 07:51:24 +0100

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_11_160937) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_11_224039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -39,6 +39,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_11_160937) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "resume_id", null: false
+    t.index ["resume_id"], name: "index_skills_on_resume_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,4 +57,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_11_160937) do
 
   add_foreign_key "projects", "resumes"
   add_foreign_key "resumes", "users"
+  add_foreign_key "skills", "resumes"
 end
