44a2f5a683d54298e13b231c51e0ad329897303f
sam <sampurna.chapagainnn@gmail.com>
2025-09-15 05:00:30 +0100

    add relationship with resume and experience,also add nested attributes for experiences

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -2,7 +2,9 @@ class Resume < ApplicationRecord
   belongs_to :user
   has_many :projects, dependent: :destroy
   has_many :skills, dependent: :destroy
+  has_many :experiences, dependent: :destroy
 
   accepts_nested_attributes_for :projects, allow_destroy: true
   accepts_nested_attributes_for :skills, allow_destroy: true
+  accepts_nested_attributes_for :experiences, allow_destroy:true
 end
