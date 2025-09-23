3fdcc418c02cc5f2f658a38181d6c02b71b4b00c
sam <sampurna.chapagainnn@gmail.com>
2025-09-23 23:24:46 +0100

    update seeds file to create projet,experience,skills

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,62 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+User.destroy_all
+Resume.destroy_all
+Skill.destroy_all
+Project.destroy_all
+Experience.destroy_all
+
+themes=['clean', 'dark', 'neon']
+
+themes.each do |theme|
+  user=User.create({
+    email: "#{theme}@example.com",
+    password: "Pass123"
+  })
+  puts "User #{user.email} created"
+
+  resume=Resume.create({
+    user: user,
+    name: Faker::Name.name,
+    bio: Faker::Markdown.emphasis,
+    theme: theme
+  })
+  puts "Resume #{resume.name} created"
+  
+  5.times do
+    skill=Skill.create({
+      resume: resume,
+      name: Faker::ProgrammingLanguage.name
+    })
+    puts "Skill #{skill.name} created"
+  end
+  
+  3.times do 
+    project=Project.create({
+      resume: resume,
+      title: Faker::Game.title,
+      summary: Faker::Quote.famous_last_words,
+      url: "www.google.com"
+    })
+  end
+
+  experience=Experience.create({
+    resume: resume,
+    position: "Software Engineer",
+    summary: "Building useful softwares to be used by different clients",
+    date_from: Date.new(2020, 1, 1),
+    date_to: Date.new(2022, 5, 1),
+    currently_working: false,
+    company_name: "Tech Corp"
+  })
+  puts "Experience #{experience.position} Created"
+
+  experience=Experience.create({
+    resume: resume,
+    position: "Web Developer",
+    summary: "Leading a team of developers to design scalable backend systems",
+    date_from: Date.new(2022, 6, 1),
+    currently_working: true,
+  })
+  puts "Experience #{experience.position} Created"
+
+end
