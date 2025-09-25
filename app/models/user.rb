e0016620c271258d5d67499793e7f82cb21bfcc8
sam <sampurna.chapagainnn@gmail.com>
2025-09-25 23:11:08 +0100

    update user has many association from has one

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :resume, dependent: :destroy
+  has_many :resumes, dependent: :destroy
 end
