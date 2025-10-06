1486cff3f6f9c317e9872568751f2f8c281b3a0a
sam <sampurna.chapagainnn@gmail.com>
2025-10-06 23:20:37 +0100

    work on payment create method and redirect to success and failure pages on specific actions

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,4 +3,24 @@ class PaymentsController < ApplicationController
     @client_token=Braintree::ClientToken.generate
   end
 
+  def create
+    nonce = params[:payment_method_nonce]
+    amount = params[:amount] || "10.00"
+
+    result = Braintree::Transaction.sale(
+      amount: amount,
+      payment_method_nonce: nonce,
+      options: {
+        submit_for_settlement: true
+      }
+    )
+
+    if result.success?
+      redirect_to payment_success_path(transaction_id: result.transaction.id)
+    else
+      Rails.logger.error("Braintree error: #{result.errors.map(&:message).join(', ')}")
+      redirect_to payment_failure_path
+    end
+  end
+
 end
